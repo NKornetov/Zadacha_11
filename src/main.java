84d3a55f3bea918486e96235bbde4dc208446de9
Nikolay Kornetov <nkorntov@yandex.ru>
2019-07-27 10:57:46 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args)
+    {
+
+        Scanner in = new Scanner(System.in);
+
+        String text = in.next();
+
+        System.out.println(text);
+    }
+}
